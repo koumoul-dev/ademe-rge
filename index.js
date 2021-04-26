13d5ce87b6ace2017eb6fda897c42a07d37d93a6
Alban Mouton <alban.mouton@gmail.com>
2021-04-26 15:25:04 +0200

    feat: better way of managing dataset

--- index.js
+++ index.js
@@ -2,47 +2,29 @@
 exports.preserveDir = true
 
 // main execution method
-exports.run = async ({ pluginConfig, processingConfig, processingId, dir, axios, log }) => {
+exports.run = async ({ pluginConfig, processingConfig, processingId, dir, axios, log, patchConfig }) => {
   const fs = require('fs-extra')
   const path = require('path')
 
   const datasetSchema = require('./resources/schema.json')
   let dataset
 
-  await log.step('Vérification du jeu de données')
-  await log.info(`tentative de lecture du jeu ${processingConfig.dataset.id}`)
-  try {
-    dataset = (await axios.get(`api/v1/datasets/${processingConfig.dataset.id}`)).data
-    await log.debug('détail du jeu de données', dataset)
-    if (dataset.extras && dataset.extras.processingId === processingId) {
-      await log.info('le jeu de données existe et est rattaché à ce traitement')
-    } else {
-      if (processingConfig.dataset.overwrite) {
-        await log.warning('le jeu de données existe et n\'est pas rattaché à ce traitement, l\'option "Surcharger un jeu existant" étant active le traitement peut continuer.')
-        dataset = (await axios.patch(`api/v1/datasets/${processingConfig.dataset.id}`, {
-          extras: { ...dataset.extras, processingId }
-        })).data
-      } else {
-        throw new Error('le jeu de données existe et n\'est pas rattaché à ce traitement')
-      }
-    }
-    dataset = (await axios.patch(`api/v1/datasets/${processingConfig.dataset.id}`, {
-      title: processingConfig.dataset.title,
-      schema: datasetSchema
-    })).data
-  } catch (err) {
-    if (err.status !== 404) throw err
-    await log.info('le jeu de données n\'existe pas encore')
-
+  if (processingConfig.datasetMode === 'create') {
     await log.step('Création du jeu de données')
-    dataset = (await axios.put(`api/v1/datasets/${processingConfig.dataset.id}`, {
+    dataset = (await axios.post('api/v1/datasets', {
+      id: processingConfig.dataset.id,
       title: processingConfig.dataset.title,
       isRest: true,
       schema: datasetSchema,
       extras: { processingId }
     })).data
-    await log.info('Le jeu de données a été créé')
-    await log.debug('jeu de données créé', dataset)
+    await log.info(`jeu de donnée créé, id="${dataset.id}", title="${dataset.title}"`)
+    await patchConfig({ datasetMode: 'update', dataset: { id: dataset.id, title: dataset.title } })
+  } else if (processingConfig.datasetMode === 'update') {
+    await log.step('Vérification du jeu de données')
+    dataset = (await axios.get(`api/v1/datasets/${processingConfig.dataset.id}`)).data
+    if (!dataset) throw new Error(`le jeu de données n'existe pas, id${processingConfig.dataset.id}`)
+    await log.info(`le jeu de donnée existe, id="${dataset.id}", title="${dataset.title}"`)
   }
 
   await log.step('Connexion au serveur FTP')
